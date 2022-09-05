c2411914f80f2608864b3b737cceb8799cf8e1eb
hoyeol0513 <hoyeol0513@naver.com>
2022-09-05 12:44:45 +0900

    Router settings in index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import About from './routes/About';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
