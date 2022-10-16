b7676e9dc84d5e9f163e23539e454c9aeb794dc3
hoyeol0513 <hoyeol0513@naver.com>
2022-10-16 14:16:13 +0900

    index.js 수정 및 recoil 추가

--- src/App.js
+++ src/App.js
@@ -1,21 +1,7 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./routes/Home";
-import About from "./routes/About";
-import Join from "./routes/Join";
-import Login from "./routes/Login";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/about/:id" element={<About />}></Route>
-        <Route path="/join" element={<Join />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-      </Routes>
-    </Router>
-  );
+  return <div></div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Join from "./routes/Join";
+import Login from "./routes/Login";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about/:id" element={<About />}></Route>
+        <Route path="/join" element={<Join />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
